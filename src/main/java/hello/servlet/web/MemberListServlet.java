ed0ca544c3911844b0f596d3cc227c074be8f2ac
doit-zero <131241458+doit-zero@users.noreply.github.com>
2024-03-27 23:58:36 +0900

    JSP and MVC

---
+++ src/main/java/hello/servlet/web/MemberListServlet.java
@@ -0,0 +1,21 @@
+package hello.servlet.web;
+
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "memberListServlet",urlPatterns = "/servlet/members")
+public class MemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
